3bbe4c8de7efcf4aa427b721eb1a3f6b5632df0e
Ava Doty <adoty@Avas-MacBook-Air-2.local>
2025-01-30 15:01:41 -0700

    Updating ReadFile

---
+++ Homeworks/Homework2/.ipynb_checkpoints/ReadFile-checkpoint.py
@@ -0,0 +1,23 @@
+import numpy as np
+import astropy.units as u
+#Importing necessary packages
+
+def Read(filename):
+#Function that reads in a file
+  file = open(filename, 'r')
+  #Opening file
+  
+  line1 = file.readline()
+  label, value = line1.split()
+  time = float(value)*u.Myr
+  #Reading in first line
+  
+  line2 = file.readline()
+  label, value = line2.split()
+  N = float(value)
+    #Should be in moles?
+  #Reading in second line
+
+  file.close()
+  data = np.genfromtxt(filename, dtype = None, names = True, skip_header = 3)
+  return time, N, data
--- Homeworks/Homework2/ReadFile.py
+++ Homeworks/Homework2/ReadFile.py
@@ -4,20 +4,28 @@ import astropy.units as u
 
 def Read(filename):
 #Function that reads in a file
-  file = open(filename, 'r')
-  #Opening file
-  
-  line1 = file.readline()
-  label, value = line1.split()
-  time = float(value)*u.Myr
-  #Reading in first line
-  
-  line2 = file.readline()
-  label, value = line2.split()
-  N = float(value)
-    #Should be in moles?
-  #Reading in second line
+#Input: A file from Milky Way data
+#Output: Returning time, total particle number, and 
+    file = open(filename, 'r')
+    #Opening file
+    
+    line1 = file.readline()
+    label, value = line1.split()
+    time = float(value)*u.Myr
+    #Reading in first line
+        #time in Myr
+    
+    line2 = file.readline()
+    label, value = line2.split()
+    N = float(value)
+    #Reading in second line
+        #total number of particles, unitless
 
-  file.close()
-  data = np.genfromtxt(filename, dtype = None, names = True, skip_header = 3)
-  return time, N, data
+    file.close()
+    #closing file
+    
+    data = np.genfromtxt(filename, dtype = None, names = True, skip_header = 3)
+    #storing remainder of data in file into an array
+    
+    return time, N, data
+    #returning time, total number of particles, and data array
